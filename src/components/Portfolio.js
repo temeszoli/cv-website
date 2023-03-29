534c9c18df81e1678b282b78e705d49b8dcea425
Zoltan <temeszoli18@gmail.com>
2023-03-29 23:06:30 +0200

    második repora mutató link módosítva

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -15,7 +15,7 @@ export default function Portfolio(){
         <div className="portfolio-container w-1/2 m-auto mb-20">
             <h1 className='text-center my-5 text-4xl bg-blue-500 m-auto rounded py-2 mt-10'>Gyakorló projektem</h1>
             <p className="text-justify mb-2">A forráskód elérhető a <a href='https://github.com/temeszoli/coffee-house'>coffee-house</a> nevű public repositorymban. A kód letöltve nem futtatható, mert a firebase authentikációs apihoz tartozó configot nem töltöttem fel.</p>
-            <p  className="text-justify mb-2">Ennek a weboldalnak a forráskódja a <a href="https://github.com/temeszoli/cvrepo">cvrepo</a> nevű repoban található.</p>
+            <p  className="text-justify mb-2">Ennek a weboldalnak a forráskódja a <a href="https://github.com/temeszoli/cv-website">cv-website</a> nevű repoban található.</p>
                     <Swiper
                 pagination={{
                 type: "fraction",

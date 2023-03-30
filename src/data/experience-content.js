8393517ade24a41f68824bb90324340751f5ad67
Zoltan <temeszoli18@gmail.com>
2023-03-30 20:22:47 +0200

    Reszponzivitás pipa

--- src/data/experience-content.js
+++ src/data/experience-content.js
@@ -30,10 +30,10 @@ export const jobs = [{
     note : 'Alkalmi munkákat vállaltam a BSI által szervezett SPAR félmaratonokon, mint szervező, szintén a középiskola alatt, diákmunkaként.'
 },{
     job : 'Support Staff, Camp Ozark - USA, Arkansas (2017)',
-    note : 'A konyhai személyzet részeként dolgoztam egy nyári táborban, Arkansasban, májustól szeptemberig.'
+    note : 'A konyhai személyzet tagjaként dolgoztam egy nyári táborban, Arkansasban, májustól szeptemberig.'
 },{
     job : 'Support Staff, Camp Canadensis - USA, Pennsylvania (2019)',
-    note : 'A konyhai személyzet részeként dolgoztam egy nyári táborban, Pennsylvaniában, májustól szeptemberig.'
+    note : 'A konyhai személyzet tagjaként dolgoztam egy nyári táborban, Pennsylvaniában, májustól szeptemberig.'
 },{
     job : 'Frontend fejlesztő gyakornok, Lanoga Kft. - Pécs (2021)',
     note : `Az egyetem által előírt kötelező szakmai gyakorlatot végeztem el ennél a cégnél, a feladatom az volt, hogy a South Park rajzfilm weboldalát hozzam létre 0 nulláról, bootstrapet használva. Ez addig a pontig sikerült, amíg nem kellett adatokat betöltenem a szerver oldalról, ahol a következő problémába ütköztem: Az alkalmazott aki az általam használni kívánt szerver oldali kódot írta, nem dolgozott már ott, voltak benne hibák, amelyeket az ott dolgozók nem tudtak megoldani, így ugyan küllemre sikerült megoldani a rám szabott feladatot, funkcionalitását tekintve viszont nem lett kész az oldal. A gyakorlati idő végefelé a mondat ami megütötte a fülemet a következő volt: "Nem mutattátok meg neki, hogy hogyan éri el az adatokat Postmannel?". Akkor még nem tudtam ez mit jelent, ma már igen.`

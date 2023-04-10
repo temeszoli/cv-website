30a1d46e43eef93442f40369995f004bbec1e980
Zoltan <temeszoli18@gmail.com>
2023-04-10 22:07:45 +0200

    exp-content refactor

--- src/data/experience-content.js
+++ src/data/experience-content.js
@@ -36,5 +36,5 @@ export const jobs = [{
     note : 'A konyhai személyzet tagjaként dolgoztam egy nyári táborban, Pennsylvaniában, májustól szeptemberig.'
 },{
     job : 'Frontend fejlesztő gyakornok, Lanoga Kft. - Pécs (2021)',
-    note : `Az egyetem által előírt kötelező szakmai gyakorlatot végeztem el ennél a cégnél, a feladatom az volt, hogy a South Park rajzfilm weboldalát hozzam létre nulláról, bootstrapet használva. Ez addig a pontig sikerült, amíg nem kellett adatokat betöltenem a szerver oldalról, ahol a következő problémába ütköztem: Az alkalmazott aki az általam használni kívánt szerver oldali kódot írta, nem dolgozott már ott, voltak benne hibák, amelyeket az ott dolgozók nem tudtak megoldani, így ugyan küllemre sikerült megoldani a rám szabott feladatot, funkcionalitását tekintve viszont nem lett kész az oldal. A gyakorlati idő vége felé a mondat ami megütötte a fülemet a következő volt: "Nem mutattátok meg neki, hogy hogyan éri el az adatokat Postmannel?". Akkor még nem tudtam ez mit jelent, ma már igen.`
+    note : `Az egyetem által előírt kötelező szakmai gyakorlatot végeztem el ennél a cégnél, a feladatom az volt, hogy a South Park rajzfilm weboldalát hozzam létre nulláról, html-t, css-t, javascriptet és bootstrapet használva.`
 }]
